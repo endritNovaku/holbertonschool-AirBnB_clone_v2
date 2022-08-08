298ae02a459c5f81e42de73f37c379f6a50bbbb4
endritNovaku <ennald@outlook.com>
2022-08-08 01:27:29 -0700

    fixing create

--- console.py
+++ console.py
@@ -124,22 +124,23 @@ class HBNBCommand(cmd.Cmd):
             return
         new_instance = HBNBCommand.classes[arg[0]]()
         """The option to add a new value to the dictionary"""
-        if len(arg) > 1:
-            value = arg[1].split("=")
-            """remove the '"' from the beggining and the end of the string"""
-            if value[1][0] == "\"":
-                value[1] = value[1][:0] + "" + value[1][1:]
-            if value[1][len(value[1]) - 1]:
-                value[1] = value[1][:len(value[1]) - 1] + ""
-            """replace '_' with space"""
-            value[1] = value[1].replace("_", " ")
-            """check if the string can be converted to number if yes convert"""
-            if value[1].isnumeric():
-                if int(value[1]):
-                    value[1] = int(value[1])
-                elif float(value[1]):
-                    value[1] = float(value[1])
-            setattr(new_instance, value[0], value[1])
+        for i in range(1, len(arg)):
+            if len(arg) > 1:
+                value = arg[i].split("=")
+                """remove the '"' from the beggining and the end of the string"""
+                if value[1][0] == "\"":
+                    value[1] = value[1][:0] + "" + value[1][1:]
+                if value[1][len(value[1]) - 1]:
+                    value[1] = value[1][:len(value[1]) - 1] + ""
+                """replace '_' with space"""
+                value[1] = value[1].replace("_", " ")
+                """check if the string can be converted to number if yes convert"""
+                if value[1].isnumeric():
+                    if int(value[1]):
+                        value[1] = int(value[1])
+                    elif float(value[1]):
+                        value[1] = float(value[1])
+                setattr(new_instance, value[0], value[1])
         storage.save()
         print(new_instance.id)
         storage.save()
