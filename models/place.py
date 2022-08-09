dab039a112b006883dd18ed191e4c04f7118efa1
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-09 04:05:13 -0700

    Fix

--- models/place.py
+++ models/place.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.review import Review
+from models.amenity import Amenity
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
-
+from os import getenv
 
 class Place(BaseModel, Base):
     """ A place to stay """
