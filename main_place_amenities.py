c47d4da1e090a71a1ebaa14372ac45bc98f9c865
KristiSeraj <serajkristi@gmail.com>
2022-08-10 02:27:26 -0700

    fix

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -4,26 +4,30 @@
 from models import *
 
 
+# creation of a State
+state = state.State(name="Italy")
+state.save()
+
 # creation of a City
-city = City(state_id=state.id, name="San Francisco")
+city = city.City(state_id=state.id, name="Milano")
 city.save()
 
 # creation of a User
-user = User(email="john@snow.com", password="johnpwd")
+user = user.User(email="bob@example.com", password="bob")
 user.save()
 
 # creation of 2 Places
-place_1 = Place(user_id=user.id, city_id=city.id, name="House 1")
+place_1 = place.Place(user_id=user.id, city_id=city.id, name="House 3")
 place_1.save()
-place_2 = Place(user_id=user.id, city_id=city.id, name="House 2")
+place_2 = place.Place(user_id=user.id, city_id=city.id, name="MyHouse1")
 place_2.save()
 
 # creation of 3 various Amenity
-amenity_1 = Amenity(name="Wifi")
+amenity_1 = amenity.Amenity(name="NoWifi")
 amenity_1.save()
-amenity_2 = Amenity(name="Cable")
+amenity_2 = amenity.Amenity(name="NOCable")
 amenity_2.save()
-amenity_3 = Amenity(name="Oven")
+amenity_3 = amenity.Amenity(name="NoOven")
 amenity_3.save()
 
 # link place_1 with 2 amenities
