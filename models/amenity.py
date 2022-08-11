706f7a97fb5e66ac0739d256efcec3957098affc
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-11 02:24:07 -0700

    Pycodestyle fix for models/state and models/amenity

--- models/amenity.py
+++ models/amenity.py
@@ -11,4 +11,4 @@ class Amenity(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     place_amenities = relationship('Place', secondary='place_amenity',
-                                    viewonly=False)
+                                   viewonly=False)
