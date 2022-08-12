a24fe5170330542f8efe45c57aec7f3ff9ab4e26
KristiSeraj <serajkristi@gmail.com>
2022-08-12 11:53:01 -0700

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -18,11 +18,10 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
@@ -33,7 +32,7 @@ class BaseModel:
         """Returns a string representation of the instance"""
         dictionary = self.__dict__.copy()
         dictionary.pop("_sa_instance_state", None)
-        return '[{}] ({}) {}'.format(type(self), self.id, dictionary)
+        return '[{}] ({}) {}'.format(type(self).__name__, self.id, dictionary)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
