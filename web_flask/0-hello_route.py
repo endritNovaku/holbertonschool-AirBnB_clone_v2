c9d00d366cf077a5cf0ba0a8809d686cb93b5b42
Arenc10 <arenc.palluqi@yahoo.com>
2022-09-08 02:18:54 -0700

    Fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """Function that returns a simple string"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
