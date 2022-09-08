608e01ac86cbfe2f0261e996f5353dd53db5161c
Arenc10 <arenc.palluqi@yahoo.com>
2022-09-08 02:15:11 -0700

    First flask app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Importing the needed framework"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Function that returns a simple string"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
