7ae03bf841af7b339f40d104261f7d1c9197b763
KristiSeraj <serajkristi@gmail.com>
2022-09-09 04:01:20 -0700

    fixed pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,6 +7,7 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route("/states_list", strict_slashes=False)
 def display_states():
     """Render state_list html page to display States created"""
