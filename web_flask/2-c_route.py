51bf9a3fb64e76f031ee9a7a8a8cd428e773eb61
KristiSeraj <serajkristi@gmail.com>
2022-09-08 23:15:27 -0700

    add task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-"""
-Importing the needed framework
-A script that starts a Flask web application
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
 """
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
-    """Function that returns a simple string"""
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hello_hbnb():
-    """Function that returns HBNB string"""
+def hbnb():
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c_print(text):
-    """Function that returns a C string"""
-    result = text.replace('_', ' ')
-    return f"C {result}"
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
