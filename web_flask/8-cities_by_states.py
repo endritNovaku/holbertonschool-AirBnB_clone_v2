88863f61809b3de2bc0bda324f82b1b364de4c82
KristiSeraj <serajkristi@gmail.com>
2022-09-09 05:06:49 -0700

    test

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,23 +2,14 @@
 """Importing Flask to run the web app"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.city import City
 
 
 app = Flask(__name__)
 
 
-"""@app.route("/states_list", strict_slashes=False)
-def display_states():
-    #Render state_list html page to display States created
-    states = storage.all()
-    return render_template('7-states_list.html', states=states)"""
-
-
 @app.route("/cities_by_states", strict_slashes=False)
-def display_city():
-    states = storage.all(State)
+def cities_by_state():
+    states = storage.all("State")
     return render_template('8-cities_by_states.html', states=states)
 
 
