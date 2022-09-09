cc49d701b34cd37aa6cb2203f7973de7a1a54825
KristiSeraj <serajkristi@gmail.com>
2022-09-09 06:04:15 -0700

    testing

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
+""" Script that starts a Flask web application
+ listened on 0.0.0.0 port 5000 using storage and routing
+ states_list
+"""
+
 from flask import Flask, render_template
 from models import storage
-
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_state():
-    states = storage.all("State")
-    return render_template('8-cities_by_states.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown(self):
-    """Method to remove current SQLAlchemy Session"""
+def close(self):
+    """ closing Session """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ display a HTML page nside the tag BODY, H1 tag for
+    States, UL tag with the list of all State objects present
+    in DBStorage sorted by name (A-Z). And LI tag for the
+    description of one State:id:name, also LI tag for city id and name.
+    Check on file 8-cities_by_states.html on templates"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
