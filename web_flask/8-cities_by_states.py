655b1c23766bd5091d7851b5e230b2effd4f98ef
KristiSeraj <serajkristi@gmail.com>
2022-09-09 04:49:56 -0700

    fixed error when displaying city and states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,16 +9,16 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+"""@app.route("/states_list", strict_slashes=False)
 def display_states():
-    """Render state_list html page to display States created"""
+    #Render state_list html page to display States created
     states = storage.all()
-    return render_template('7-states_list.html', states=states)
+    return render_template('7-states_list.html', states=states)"""
 
 
 @app.route("/cities_by_states", strict_slashes=False)
 def display_city():
-    states = storage.all()
+    states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
 
